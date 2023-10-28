ae1ae787af4745b9b197dbffd50c18f89aacff21
gostkiu <vp0600037@gmail.com>
2023-10-28 18:43:25 +0300

    fin

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ class Racet(pygame.sprite.Sprite):
         self.speed = speed
     def update_l(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] and self.rect.y < HEIGHT-100:
             self.rect.y += self.speed
     def update_r(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_UP]and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN]and self.rect.y < HEIGHT-100:
             self.rect.y += self.speed
     def reset(self):
         w.blit(self.image,(self.rect.x,self.rect.y))
@@ -47,6 +47,10 @@ backgraund = pygame.transform.scale(pygame.image.load("skyline.jpg"),SIZE)
 clock = pygame.time.Clock()
 pygame.font.init()
 font1 = pygame.font.Font(None,60)
+font2 = pygame.font.Font(None,45)
+
+score_l = 0
+score_r = 0
 
 run = True
 fin = False
@@ -70,13 +74,24 @@ while run:
             or pygame.sprite.collide_rect(bal,racet_r):
             speed_x = -speed_x
         if bal.rect.x <= -50:
+            score_r+= 1
+            bal.rect.center = WIDTH/2,HEIGHT/2
+
+        if score_r >= 5:
             fin = True
             text = font1.render("Лол",True,(255,0,0))
             w.blit(text,(WIDTH/2,HEIGHT/2))
         if bal.rect.x >= WIDTH+50:
+            score_l+= 1
+            bal.rect.center = WIDTH/2,HEIGHT/2
+        if score_l >= 5:
             fin = True
             text = font1.render("Лол",True,(255,0,0))
             w.blit(text,(WIDTH/2,HEIGHT/2))
+        score_r_t = font2.render(str(score_r),True,(255,0,0))
+        score_l_t = font2.render(str(score_l),True,(255,0,0))
+        w.blit(score_l_t,(WIDTH/4,50))
+        w.blit(score_r_t,(WIDTH*3/4,50))
     pygame.display.update()
     clock.tick(FPS)
 
